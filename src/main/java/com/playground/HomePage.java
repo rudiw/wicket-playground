4aa769af646b84cb6f012392503063b712accc2d
Rudi Wijaya <rudi.mail90@gmail.com>
2024-01-22 22:36:34 +0700

    2. logger

--- src/main/java/com/playground/HomePage.java
+++ src/main/java/com/playground/HomePage.java
@@ -3,12 +3,17 @@ package com.playground;
 import org.apache.wicket.request.mapper.parameter.PageParameters;
 import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.markup.html.WebPage;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class HomePage extends WebPage {
 	private static final long serialVersionUID = 1L;
 
+	private static final Logger LOG = LoggerFactory.getLogger(HomePage.class);
+
 	public HomePage(final PageParameters parameters) {
 		super(parameters);
+		LOG.info("Loading HomePage");
 
 		add(new Label("version", getApplication().getFrameworkSettings().getVersion()));
 
